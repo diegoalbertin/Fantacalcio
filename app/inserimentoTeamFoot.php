6ad5edc69462fe0f58aa094289353c61e251c765
diegoalbertin <albertindiego08@gmail.com>
2023-03-14 23:18:43 +0100

    aggiunta funzioni di limite della selezone checkbox, elenco giocatori selezionati nella pagina di creazione squadre,manca solo collegamneto api

--- app/inserimentoTeamFoot.php
+++ app/inserimentoTeamFoot.php
@@ -20,7 +20,7 @@ $_SESSION['totalTeam']=count($team);
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (isset($_POST['create'])) {
-
+        echo "a";
         if($_SESSION["n-team"]<=$_SESSION["totalTeam"]){
 
             if(count($_POST['foot'])!=15){
@@ -29,12 +29,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     "id-user"=>$_SESSION["n-team"]);
                 $_SESSION["data"][]=$data;
                 var_dump($_SESSION["data"]);
-                //$_SESSION["n-team"] =intval($_SESSION["n-team"])+1;       
+                $_SESSION["n-team"] =intval($_SESSION["n-team"])+1;       
             }
             else
             {
                 echo '<script>alert("!! Campi incompleti !!")</script>';
             }
+        }
+        else if($_SESSION["n-team"]==$_SESSION["totalTeam"]){
+            
         }
     }
 }
@@ -51,39 +54,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </head>
     <body>
         <div class="row">
-            <div class="box-selectedFoot" style="background-color: grey; min-height:30px;">
-
+            <div class=" text-center form-container box-selectedFoot rounded col-6 offset-3">
+            <h1>giocatori selezionati:</h1>
+                <p id="p-selectedFoot" class="text-center"></p>
             </div>
         </div>
         <div class="row">
-            <div><h1 onclick="hideShow()">></h1></div>
-                <div class="form-container bordered rounded col-8 offset-2" id="div-form" style="display:block;">
-                    <form class="text-center" method="post" name="form-team" action="">
-                        <div class="row">
-                            <label class="bold" for="text">team-<?php if(isset($_SESSION["n-team"])){
-                                echo $team[intval($_SESSION['n-team'])]->name;}?></label>
-                            <div class="text-center">
-                                <?php foreach($foot as $f){?>
-                                    
-                                <div class="row">
-                                    <div class="form-element">
-                                    <input class="col-3 foot" name="foot[]" style="" onclick="selectFoot(<?php echo $f->id; ?>,'<?php echo $f->name; ?>')" type="checkbox" value="<?php echo $f->id; ?>">
-                                    <label class="text-center col-3 offset-1" for="<?php echo $f->name; ?>"><?php echo $f->name; ?></label>                              
-                                    <label class="text-center col-3 " for="text"><?php echo $f->team; ?></label>                              
+            <div class="footList">
+                <div class="row">
+                    <div class="col-2 " ><button class="rounded col-2 offset-10" onclick="hideShow()">></button></div>
+                    <div class="form-container bordered rounded col-8 " id="div-form" style="display:block;">
+                        <form class="text-center" method="post" name="form-team" action="">
+                            <div class="row">
+                                <h1>team-<?php if(isset($_SESSION["n-team"])){
+                                    echo $team[intval($_SESSION['n-team'])]->name;}?></h1>
+                                <input type="submit" class="submit rounded col-2 offset-5" style="background-color:#989898;" onclick="checkPlayerNumber(event)" name="create" value="crea">
+
+                                <div class="text-center">
+                                    <?php foreach($foot as $f){?>
+                                        
+                                    <div class="row">
+                                        <div class="form-element">
+                                        <input class="col-3 foot" name="foot[]" onclick="selectFoot(<?php echo $f->id; ?>,'<?php echo $f->name; ?>')" type="checkbox" value="<?php echo $f->id; ?>">
+                                        <label class="text-center col-3 offset-1" for="<?php echo $f->name; ?>"><?php echo $f->name; ?></label>                              
+                                        <label class="text-center col-3 " for="text"><?php echo $f->team; ?></label>                              
+                                        </div>
                                     </div>
+                                    <?php }?>
                                 </div>
-                                <?php }?>
-                                <input type="submit" name="create" value="crea">
-                            </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div> 
+            </div>
+        </div> 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script>
         function hideShow(){
             const elem = document.getElementById('div-form');
-            console.log(elem.style.display);
 
             if(elem.style.display=='none!important'||elem.style.display=='none'){
                 elem.style.display='block';
@@ -98,12 +106,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         let delName="";
 
         function selectFoot(id, name){
-            if(idSelectedFoot.includes(id)==false||idSelectedFoot.length==0||idSelectedFoot.length<=15){
+            if(idSelectedFoot.includes(id)==false){
 
                 idSelectedFoot.push(id);
                 nameSelectedFoot.push(name);
-
-            }else if(idSelectedFoot.includes(id)==true||idSelectedFoot.length>0||idSelectedFoot.length<=15){
+            }else if(idSelectedFoot.includes(id)==true){
                 delId=id;
                 delName=name;
 
@@ -112,7 +119,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 idSelectedFoot.splice(0,idSelectedFoot.length);
                 nameSelectedFoot.splice(0,nameSelectedFoot.length);
-
                 for( i=0;i<tmpId.length;i++){
                     idSelectedFoot.push(tmpId[i]);
                     nameSelectedFoot.push(tmpName[i]);
@@ -121,6 +127,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 delId=0;
                 delName="";
             }
+            displaySelectedFoot();
         }
 
         function filterId(id){
@@ -133,29 +140,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
 
         function displaySelectedFoot(){
-            for(i=0;i<nameSelectedFoot.length;i++){
+            var txt = "";
 
+            for(const foot of nameSelectedFoot){
+                txt+="-"+foot+"<br>";
             }
+            document.getElementById('p-selectedFoot').innerHTML=txt;
         }
+
         //disabilitare click dop 15 selezioni
         const input = document.querySelectorAll('.foot');
+        const submit = document.querySelectorAll('.submit');
 
         for(const inp of input){
             inp.addEventListener('click',updateDisplay );
         }
+
         function updateDisplay() {
-        let checkedCount = 0;
-        for (const inp of input) {
-            if (inp.checked) {
-            checkedCount++;
-            }
-        }
-        console.log(checkedCount);
-        if(checkedCount==15){
+        if(count()==15){
             disable_cb();
+            submit[0].style.backgroundColor = '#DCDCDC';
+
         }else{
+            console.log(submit);
+            submit[0].style.backgroundColor = '#989898';
             enable_cb();
         }
+        }
+
+        function count(){
+            let checkedCount = 0;
+            for (const inp of input) {
+                if (inp.checked) {
+                checkedCount++;
+                }
+            }
+            return checkedCount;
         }
 
         function disable_cb(){
@@ -168,6 +188,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         function enable_cb(){
             for (const inp of input) {
             inp.disabled=false;
+            }
+        }
+
+        function checkPlayerNumber(e){
+            if(count()!=15){
+                e.preventDefault();
             }
         }
 
